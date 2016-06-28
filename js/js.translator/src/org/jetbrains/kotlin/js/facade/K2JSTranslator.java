5af2b1dcc044020ecc27a0ae3770143ea9fa8c03
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-06-28 17:49:17 +0300

    KT-12877: serialize information about file annotations. For each top-level declaration store containing file. Use this information to properly handle file-targeted JsModule.

--- js/js.translator/src/org/jetbrains/kotlin/js/facade/K2JSTranslator.java
+++ js/js.translator/src/org/jetbrains/kotlin/js/facade/K2JSTranslator.java
@@ -78,7 +78,7 @@ public final class K2JSTranslator {
         ModuleDescriptor moduleDescriptor = analysisResult.getModuleDescriptor();
         Diagnostics diagnostics = bindingTrace.getBindingContext().getDiagnostics();
 
-        TranslationContext context = Translation.generateAst(bindingTrace, files, mainCallParameters, config);
+        TranslationContext context = Translation.generateAst(bindingTrace, files, mainCallParameters, moduleDescriptor, config);
         ProgressIndicatorAndCompilationCanceledStatus.checkCanceled();
         if (hasError(diagnostics)) return new TranslationResult.Fail(diagnostics);
 
@@ -95,6 +95,7 @@ public final class K2JSTranslator {
         ProgressIndicatorAndCompilationCanceledStatus.checkCanceled();
 
         List<String> importedModules = new ArrayList<String>(context.getImportedModules().keySet());
-        return new TranslationResult.Success(config, files, program, diagnostics, importedModules, moduleDescriptor);
+        return new TranslationResult.Success(config, files, program, diagnostics, importedModules, moduleDescriptor,
+                                             bindingTrace.getBindingContext());
     }
 }
