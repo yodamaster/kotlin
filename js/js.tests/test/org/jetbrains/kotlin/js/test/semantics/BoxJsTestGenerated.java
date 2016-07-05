3c958fb25db4bff2c648559d5dc2c97ea747d457
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-07-05 11:45:33 +0300

    KT-12877: support plain reference to declaration in UMD wrapper, support plain reference to package

--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/BoxJsTestGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/BoxJsTestGenerated.java
@@ -5018,6 +5018,18 @@ public class BoxJsTestGenerated extends AbstractBoxJsTest {
             doTest(fileName);
         }
 
+        @TestMetadata("externalPackagePlain.kt")
+        public void testExternalPackagePlain() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("js/js.translator/testData/box/jsModule/externalPackagePlain.kt");
+            doTest(fileName);
+        }
+
+        @TestMetadata("externalPackageUmdFallback.kt")
+        public void testExternalPackageUmdFallback() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("js/js.translator/testData/box/jsModule/externalPackageUmdFallback.kt");
+            doTest(fileName);
+        }
+
         @TestMetadata("externalProperty.kt")
         public void testExternalProperty() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("js/js.translator/testData/box/jsModule/externalProperty.kt");
