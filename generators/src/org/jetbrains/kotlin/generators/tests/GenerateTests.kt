b8bf536dce40007ede184681af8554ccbb0a2aa8
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-10-18 17:27:02 +0300

    JS: remove test for reserved words

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1161,7 +1161,8 @@ fun main(args: Array<String>) {
         }
     }
 
-    generateTestDataForReservedWords()
+    // TODO: repair these tests
+    //generateTestDataForReservedWords()
 
     testGroup("js/js.tests/test", "js/js.translator/testData") {
         testClass<AbstractBoxJsTest>() {
--- js/js.translator/testData/box/reservedWords/_common.kt
+++ js/js.translator/testData/box/reservedWords/_common.kt
@@ -7,11 +7,11 @@ fun testNotRenamed(case: String, f: () -> Unit) = test(case, false, f)
 
 fun test(keyword: String, expectedRenamed: Boolean, f: Any) {
     val fs = f.toString().replace("while (false)", "")
-    val matches = Regex("[\\w$]*$keyword[\\w_$]*").matchAll(fs).map { it.value }.toList()
+    val matches = Regex("[\\w$]*$keyword(_[\\w]+\\$?)?").matchAll(fs).map { it.value }.toList()
 
     assertNotEquals(0, matches.size, "matches is empty for fs = $fs")
 
-    val actual = matches.last()
+    val actual = matches.last().removeSuffix("_getInstance")
 
     assertTrue(actual.contains(keyword), "'$keyword' not found in '$matches' from '$fs'")
 
