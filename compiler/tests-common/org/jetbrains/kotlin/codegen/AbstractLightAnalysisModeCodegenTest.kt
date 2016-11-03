a074a4ae799fac2bca0c2fa6055803e67b36c646
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2016-11-03 04:30:20 +0300

    Enable light class analysis mode for AbstractLightAnalysisModeCodegenTest. See also the previous commit. Note that no test data files are changed, it means that light classes with the light analysis mode enabled are the same as with the full analysis.

--- compiler/tests-common/org/jetbrains/kotlin/codegen/AbstractLightAnalysisModeCodegenTest.kt
+++ compiler/tests-common/org/jetbrains/kotlin/codegen/AbstractLightAnalysisModeCodegenTest.kt
@@ -16,11 +16,27 @@
 
 package org.jetbrains.kotlin.codegen
 
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.BindingTrace
+import org.jetbrains.kotlin.resolve.TopDownAnalysisMode
+import org.jetbrains.kotlin.resolve.jvm.extensions.AnalysisHandlerExtension
 import java.io.File
 
 abstract class AbstractLightAnalysisModeCodegenTest : AbstractBytecodeListingTest() {
     override val classBuilderFactory: ClassBuilderFactory
         get() = ClassBuilderFactories.TEST_LIGHT_CLASSES
+
+    override fun setupEnvironment(environment: KotlinCoreEnvironment) {
+        AnalysisHandlerExtension.registerExtension(environment.project, object : AnalysisHandlerExtension {
+            override fun beforeAnalysis(project: Project, module: ModuleDescriptor, files: Collection<KtFile>, bindingTrace: BindingTrace) {
+                bindingTrace.record(BindingContext.TOP_DOWN_ANALYSIS_MODE, Unit, TopDownAnalysisMode.Light)
+            }
+        })
+    }
 
     override fun getTextFile(ktFile: File): File {
         val boxTestsDir = File("compiler/testData/codegen/box")
