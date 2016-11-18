02772ad0335226d9011ae53cabe49fcbd0d6ea86
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-11-18 16:29:24 +0100

    Process IGNORE_BACKEND directive in android tests

--- compiler/android-tests/tests/org/jetbrains/kotlin/android/tests/CodegenTestsOnAndroidGenerator.java
+++ compiler/android-tests/tests/org/jetbrains/kotlin/android/tests/CodegenTestsOnAndroidGenerator.java
@@ -243,7 +243,7 @@ public class CodegenTestsOnAndroidGenerator extends KtUsefulTestCase {
             else {
                 String fullFileText = FileUtil.loadFile(file, true);
 
-                if (!hasJvmBackendTarget(fullFileText)) {
+                if (ignoreJvmBackendTarget(fullFileText) || !hasJvmBackendTarget(fullFileText)) {
                     continue;
                 }
 
@@ -270,6 +270,21 @@ public class CodegenTestsOnAndroidGenerator extends KtUsefulTestCase {
 
     private static boolean hasBoxMethod(String text) {
         return text.contains("fun box()");
+    }
+
+    private static boolean ignoreJvmBackendTarget(String text) {
+        List<String> ignore = InTextDirectivesUtils.findLinesWithPrefixesRemoved(text, "// IGNORE_BACKEND: ");
+        if (ignore.isEmpty()) {
+            return false;
+        }
+
+        for (String backend : ignore) {
+            if (backend.equals("JVM")) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private static boolean hasJvmBackendTarget(String text) {
